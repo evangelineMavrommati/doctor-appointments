c54badfedbba3fba456c994305174de076738e27
evangelineMavrommati <emavro19@gmail.com>
2020-07-06 16:52:15 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,19 @@ doctors = Doctor.create([
   { first_name: "Sharon", last_name: "Carter", email: "carter@notablehealth.com" },
   { first_name: "Albert", last_name: "Einstein", email: "einstein@notablehealth.com" },
 ])
+
+3.times do
+  doctors.first.appointments.create(
+    time: "07-06-2020 9:30 AM",
+    kind: "New Patient",
+    patient_first_name: "Evangeline",
+    patient_last_name: "Mavrommati"
+  )
+end
+
+doctors.third.appointments.create(
+  time: "07-09-2020 10:30 AM",
+  kind: "Follow-up",
+  patient_first_name: "Tristan",
+  patient_last_name: "Brooks"
+)
